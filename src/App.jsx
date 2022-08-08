7642c4bc1904187881a0f4c446333e1a0f9e8714
Leonardo Ferreira Rochedo <learnir@outlook.com.br>
2022-08-07 22:17:17 -0300

    HTML e CSS rodando, falta apenas adicionar o relogio

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
+import { useState } from 'react'
 import './App.css'
 
 export function App() {
+
+  const [date, setDate] = useState("00 : 00 : 00 : 00");
+
   var dataAtual = new Date();
+
   var dia = dataAtual.getDate();
   var horas = dataAtual.getHours();
   var minutos = dataAtual.getMinutes();
   var seconds = dataAtual.getSeconds();
-  let date = "0" + dia + " : " + horas + " : " + minutos + " : " + seconds;
+
+  // setInterval(() => {
+  //   setDate(dia + " : " + horas + " : " + minutos + " : " + seconds-2);
+  // }, 1000);
+
+  function showMenu() {
+    const element = document.querySelector("#menu")
+    element.classList.remove("noOpacity");
+    element.classList.add("opacity");
+  }
+
+  function unshowMenu() {
+    const menu = document.querySelector("#menu");
+    menu.classList.remove("opacity");
+    menu.classList.add("noOpacity");
+
+    const element = document.querySelector("#finally");
+    element.classList.remove("noOpacity");
+    element.classList.add("opacity");
+  }
+
+  function closeMenu() {
+    const element = document.querySelector("#finally");
+    element.classList.remove("opacity");
+    element.classList.add("noOpacity");
+  }
 
   return (
     <main>
@@ -16,10 +46,20 @@ export function App() {
           <h2>{date}</h2>
         </div>
         <p>Inscreva-se para saber mais sobre o lançamento</p>
-        <button>Inscreva-se</button>
+        <button onClick={showMenu}>Inscreva-se</button>
       </div>
       <div className="rocket">
         <img src="src\assets\rocket.svg" alt="Foguete sendo lançado" />
+      </div>
+      <div className="subscribe" id='menu'>
+        <h2>Seu melhor e-mail</h2>
+        <input type="text" placeholder='seuemail@email.com'/>
+        <button onClick={unshowMenu}>Enviar</button>
+      </div>
+      <div className="verify noOpacity" id='finally'>
+        <button onClick={closeMenu}><img src="src/assets/close.svg" alt="Close button" /></button>
+        <img src="src/assets/verificado.png" alt="Símbolo de verificado" />
+        <p>E-mail cadastrado com sucesso!</p>
       </div>
     </main>
   )
